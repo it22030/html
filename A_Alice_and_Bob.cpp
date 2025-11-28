7f92b6da93fa284d2bf9d312f487f565f7b237be
ashikullah <it22030@mbstu.ac.bd>
2025-11-28 14:25:34 +0600

    Initial commit

---
+++ A_Alice_and_Bob.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+
+int main() {
+    fastio;
+    
+    int t;
+    cin >> t;
+    while (t--) {  }
+
+    return 0;
+}
